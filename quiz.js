b02b904f1546849fbb29b7bd7008f3c72a686657
Greg Korte <gregmkorte@gmail.com>
2023-11-16 14:39:49 -0600

    Writing algorithm for program

--- quiz.js
+++ quiz.js
@@ -1,23 +1,37 @@
 const readlineSync = require("readline-sync");
 
 // Function to ask a quiz question and returns a new user score that is incremented for correct answer
-function askQuestion(questionObj, userScore) {}
+function askQuestion(questionObj, userScore) {
+  // Ask the question & get user answer
+  // let answer = readlineSync.question(questionObj.question);
+  // Check user input checkAnswer(answer, questionObj.correctAnswer)
+  // if checkAnswer(answer, questionObj.correctAnswer)
+  // true userScore += 1
+  // false userScore
+  // Returns score
+}
 
 // Function to check the user's answer. Allow answer to ignore casing of correct answer. return a boolean
-function checkAnswer(userAnswer, correctAnswer) {}
+function checkAnswer(userAnswer, correctAnswer) {
+  // if (userAnswer.toLowerCase() === correctAnswer.toLowerCase())
+  // return true
+  // return false
+}
 
 // Function to run the quiz
 function runQuiz(quizQuestions) {
   let userScore = 0;
 
-  // Iterate through the questions
+  // for/of or for loop to iterate question objects
+  // Run askQuestion(quizQuestion, userScore)
+  // store return in variable (newScore)
 
   // Display final score
   console.log(
     "Quiz complete! Your score: " +
       userScore +
       " out of " +
-      quizQuestions.length,
+      quizQuestions.length
   );
 }
 
